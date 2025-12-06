d8fc3307a14ce2e348a581dc837505f72db73c91
Blockpixel Studios <blockpixelstudiosmc@gmail.com>
2025-12-05 21:17:19 -0400

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        bpblue: '#00E7D1',
+        deepblue: '#021728'
+      }
+    }
+  },
+  plugins: []
+};
