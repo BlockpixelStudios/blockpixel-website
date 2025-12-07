41d9b1cd080138a9c7618a1990e19ef4fe083444
Blockpixel Studios <blockpixelstudiosmc@gmail.com>
2025-12-07 00:58:36 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/server';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  // outras opções que você já tiver (integrations, vite, etc) podem ficar aqui
 });
