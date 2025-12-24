74e137acdefdd25786796f317bdbe5c720d50776
Blockpixel Studios <blockpixelstudiosmc@gmail.com>
2025-12-24 09:06:47 -0400

    Refactor BlogPostPage to use getBlogPostBySlug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,25 +1,16 @@
-import { getBlogPosts } from "../../../lib/markdown";
-import { notFound } from "next/navigation";
+import { getBlogPostBySlug } from "@/lib/markdown";
 import ReactMarkdown from "react-markdown";
 
-export default function BlogPostPage({ params }: any) {
-  const posts = getBlogPosts();
-  const post = posts.find((p: any) => p.slug === params.slug);
+export default function BlogPost({ params }) {
+  const post = getBlogPostBySlug(params.slug);
 
-  if (!post) return notFound();
+  if (!post) return null;
 
   return (
-    <main className="container" style={{ paddingTop: "60px" }}>
+    <main className="container">
       <article>
-        <h1 style={{ marginBottom: "12px" }}>{post.title}</h1>
-
-        <p style={{ color: "var(--muted)", marginBottom: "32px" }}>
-          {post.date}
-        </p>
-
-        <ReactMarkdown>
-          {post.content}
-        </ReactMarkdown>
+        <h1>{post.title}</h1>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
       </article>
     </main>
   );
